df706d66d1a20328988ac2dfc14001ed83ed1e8a
JunsuKim-3765 <84621704+JunsuKim-3765@users.noreply.github.com>
2021-06-30 21:09:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball;
+ var g1, g2, g3;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1200, 500);
+   if (keyDown("UP_ARROW")){
+	if(keyCode==UP_ARROW)
+    {
+      Matter.Body.applyForce(ball2,{x:0,y:0},{x:0.05,y:0});
+    }
+   }
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ball = Bodies.circle(50, 150, 15, {restitution:0.9});
+	World.add(world, ball);
+	rectMode(CENTER);
+	ellipseMode(RADIUS);
+
+	g1=new Ground(600, 490, 1200, 20);
+	g2=new Ground(1100, 475, 20, 99);
+	g3=new Ground(1150, 475, 20, 99);
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  g1.show();
+  g2.show();
+  g3.show();
+  ellipse(ball.position.x, ball.position.y, 15);
+
+  Engine.update(engine);
+  drawSprites();
+ 
+}
+
+
+
